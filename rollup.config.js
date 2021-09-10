e1a7f7c8eddf43ebc6f3b2d5e3a818160fce3035
gusalbukrk <gusalbukrk@gmail.com>
2021-09-10 14:31:50 -0300

    fix d.ts files output

--- rollup.config.js
+++ rollup.config.js
@@ -53,7 +53,7 @@ export default [
     ],
   },
   {
-    input: 'dist/types/src/index.d.ts',
+    input: 'dist/types/index.d.ts',
     output: [{ file: 'dist/index.d.ts', format: 'es' }],
     plugins: [
       dts(),

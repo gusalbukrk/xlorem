87bf8690da2247bc7af09027eefb9319df6d0fac
gusalbukrk <gusalbukrk@gmail.com>
2021-10-24 11:31:26 -0300

    refactor tokenize-words

--- packages/tokenize-words/src/index.spec.ts
+++ packages/tokenize-words/src/index.spec.ts
@@ -8,4 +8,24 @@ describe('tokenizeWords (main function)', () => {
 
     expect(() => tokenizeWords('')).toThrow(notEnoughKeywords);
   });
+
+  it('integration test', () => {
+    expect.assertions(1);
+
+    const x = tokenizeWords(
+      'The quux. on .NET foo.\nFoo, which qux, and quux. or foobar.By - G. A. (example.com)'
+    );
+
+    expect(x).toStrictEqual([
+      'quux.',
+      'NET',
+      'foo',
+      'foo',
+      'qux',
+      'quux.',
+      'foobar',
+      'G.A.',
+      'example.com',
+    ]);
+  });
 });

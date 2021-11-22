f527f4d75f582ba8a0408f81a16eb115b620663c
gusalbukrk <gusalbukrk@gmail.com>
2021-11-22 12:01:11 -0300

    refactor generate-random-text

--- packages/generate-random-text/src/index.ts
+++ packages/generate-random-text/src/index.ts
@@ -1,3 +1,4 @@
+import { breakdownDefault } from '@xlorem/common/src/constants';
 import {
   freqMapType,
   unitType,
@@ -9,14 +10,36 @@ import distributeWords from './distributeWords';
 import generateTextArray from './generateTextArray';
 import stringifyTextArray from './stringifyTextArray';
 
+type optionsType = {
+  unit: unitType;
+  quantity: number;
+  format: formatType;
+  breakdown: breakdownType;
+};
+
+/**
+ * Default for 'paragraphs' is 5 and for 'words' 200.
+ * If neither is supplied, defaults to 5 paragraphs.
+ */
+function getOptionsDefault(unit: unitType = 'paragraphs'): optionsType {
+  return {
+    unit,
+    quantity: unit === 'paragraphs' ? 5 : 200,
+    format: 'plain',
+    breakdown: breakdownDefault,
+  };
+}
+
 function generateText(
   freqMap: freqMapType,
-  unit: unitType,
-  quantity: number,
-  format: formatType,
-  breakdown: breakdownType,
+  options: Partial<optionsType>,
   isTest = false
 ): string | string[][][] {
+  const { unit, quantity, format, breakdown } = {
+    ...getOptionsDefault(options.unit),
+    ...options,
+  };
+
   const wordsDistribution = distributeWords(unit, quantity, breakdown);
 
   const textArray = generateTextArray(freqMap, wordsDistribution);

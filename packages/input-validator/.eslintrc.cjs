b98b7e87b0df1bed22117eff6673e3008857094d
gusalbukrk <gusalbukrk@gmail.com>
2021-12-21 20:07:46 -0300

    input-validator configure as pure typescript library

--- packages/input-validator/.eslintrc.cjs
+++ packages/input-validator/.eslintrc.cjs
@@ -1,5 +1,5 @@
 const config = require('../../.eslintrc.cjs');
 
-config.overrides[0].parserOptions.project = './tsconfig.jest.json';
+config.overrides[0].parserOptions.project = './tsconfig.json';
 
 module.exports = config;

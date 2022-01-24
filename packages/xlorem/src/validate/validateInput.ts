7388809e6e53c1c0fba992eb295ad93dbd566682
gusalbukrk <gusalbukrk@gmail.com>
2022-01-24 11:09:04 -0300

    `packages/xlorem` refactor `validateInput.ts`

--- packages/xlorem/src/validate/validateInput.ts
+++ packages/xlorem/src/validate/validateInput.ts
@@ -5,45 +5,80 @@ import {
 } from 'xlorem-common/src/errorMessages';
 import {
   inputType,
+  queryInputType,
   textInputType,
   wordsArrayInputType,
   freqMapInputType,
 } from 'xlorem-common/src/types';
 import { isObject } from 'xlorem-common/src/utils';
 
+function isInputQueryString(input: inputType): input is queryInputType {
+  return typeof input === 'string';
+}
+
+function isInputText(input: inputType): input is textInputType {
+  return (
+    isObject(input) &&
+    Object.keys(input).length === 2 &&
+    typeof (input as textInputType).title === 'string' &&
+    typeof (input as textInputType).body === 'string'
+  );
+}
+
+function isInputWordsArray(input: inputType): input is wordsArrayInputType {
+  return (
+    isObject(input) &&
+    Object.keys(input).length === 2 &&
+    typeof (input as wordsArrayInputType).title === 'string' &&
+    Array.isArray((input as wordsArrayInputType).words) &&
+    (input as wordsArrayInputType).words.every(
+      (word) => typeof word === 'string'
+    )
+  );
+}
+
+function isInputFreqMap(input: inputType): input is freqMapInputType {
+  return (
+    isObject(input) &&
+    Object.keys(input).length === 2 &&
+    typeof (input as freqMapInputType).title === 'string' &&
+    isObject((input as freqMapInputType).map) &&
+    Object.keys((input as freqMapInputType).map).every((key) =>
+      /^\d+$/.test(key)
+    ) &&
+    Object.values((input as freqMapInputType).map).every(
+      (value) =>
+        Array.isArray(value) && value.every((el) => typeof el === 'string')
+    )
+  );
+}
+
 function validateInput(input: inputType): string[] {
   const errors: string[] = [];
 
-  const isQueryString = typeof input === 'string';
+  const isQueryString = isInputQueryString(input);
+  const isText = isInputText(input);
+  const isWordsArray = isInputWordsArray(input);
+  const isFreqMap = isInputFreqMap(input);
 
-  const isText =
-    typeof input === 'object' &&
-    Object.keys(input).length === 2 &&
-    typeof input.title === 'string' &&
-    typeof (input as textInputType).body === 'string';
-
-  const isWordsArray = Array.isArray((input as wordsArrayInputType).words);
-
-  const { map } = input as freqMapInputType;
-  const isFreqMap =
-    isObject(map) &&
-    Object.keys(map).every((prop) => /^\d+$/.test(prop)) &&
-    Object.values(map).every((arr) => Array.isArray(arr));
-
-  if (!(isQueryString || isText || isWordsArray || isFreqMap)) {
+  if (
+    !(isQueryString || isText || isWordsArray || isFreqMap) // invalid input
+  ) {
     errors.push(neitherStringNorArticle);
-  }
+  } else {
+    // additional errors for specific input types
 
-  if (isQueryString && input === '') {
-    errors.push(emptyQueryString);
-  }
+    if (isQueryString && input === '') {
+      errors.push(emptyQueryString);
+    }
 
-  if (isText) {
-    const wordsQuantityMinRequired = 150;
-    const wordsQuantity = (input as textInputType).body.split(' ').length;
+    if (isText) {
+      const wordsQuantityMinRequired = 150;
+      const wordsQuantity = input.body.split(' ').length;
 
-    if (wordsQuantity < wordsQuantityMinRequired) {
-      errors.push(textTooShort);
+      if (wordsQuantity < wordsQuantityMinRequired) {
+        errors.push(textTooShort);
+      }
     }
   }
 

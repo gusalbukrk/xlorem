4eae07cad8316e8ddce0ee342e743e43d22df488
gusalbukrk <gusalbukrk@gmail.com>
2022-07-06 22:15:20 -0300

    edit `cli`

--- packages/cli/.replace-in-file.cjs
+++ packages/cli/.replace-in-file.cjs
@@ -1,9 +1,16 @@
+// previously,
 // for some unknown reason, `string_decoder` import
 // appears twice and one of them has a trailing forward slash
 // would result in an error, because relative ESM imports must use full paths
+// module.exports = {
+//   files: 'dist/bundle.js',
+//   from: /^import (.*) from 'string_decoder\/';$/gm,
+//   to: "import $1 from 'string_decoder';",
+// };
 
+// every bundle file must start with a shebang
 module.exports = {
-  files: 'dist/bundle.js',
-  from: /^import (.*) from 'string_decoder\/';$/gm,
-  to: "import $1 from 'string_decoder';",
+  files: 'dist/bundle.@(cjs|js)',
+  from: /^/,
+  to: '#!/usr/bin/env node\n\n',
 };
--- packages/cli/README.md
+++ packages/cli/README.md
@@ -1,1 +1,13 @@
-# cli
+# xlorem-cli
+
+## Install
+
+- `npm i -g xlorem-cli`
+
+## Basic usage
+
+- `xlorem-cli -Q 'harry potter' -u words -q 250 -f html`
+
+## Help
+
+- `xlorem-cli --help`
